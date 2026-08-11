5ae1680851c86e58175158a1cc4b5f27bb5b87cd
Oğuzhan Beyaz <beyazogz@gmail.com>
2026-08-12 01:16:03 +0300

    feat(api): initialize NestJS backend foundation

---
+++ apps/api/src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3001),
+      }),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
